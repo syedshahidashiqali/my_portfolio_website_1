0b1430417fa92c1a566b0e4566744286be99d7a3
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-03-30 14:57:18 +0500

    create route root route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.scss'
-
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
 function App() {
-  return <div>hello</div>
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,7 @@
+import './index.scss'
+
+const Layout = () => {
+  return <div>Hello, Layout</div>
+}
+
+export default Layout
