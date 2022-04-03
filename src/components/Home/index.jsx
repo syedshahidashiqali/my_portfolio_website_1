0e56056f7ddade89a52bc7afddae72bc5ecd0d83
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-04-03 12:47:03 +0500

    complete home component but letter animation is remaining

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,23 @@
+import "./index.scss"
+import { Link } from "react-router-dom"
+import LogoTitle from "../../assets/images/logo-s.png"
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          hahid
+          <br />
+          web developer
+        </h1>
+        <h2>Frontend Developer / JavaScript Developer</h2>
+        <Link to="/contact" className="flat-button">CONTACT ME</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
