8d5b2a494972d7b09eabf29d2e8e7f9a17ac9ebe
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2022-04-03 15:17:57 +0500

    complete draw svg animation on logo

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 import LogoTitle from "../../assets/images/logo-s.png"
 import { useState, useEffect } from "react"
 import AnimatedLetters from "../AnimatedLetters"
+import Logo from "./Logo";
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -54,6 +55,7 @@ const Home = () => {
         <h2>Frontend Developer / JavaScript Developer</h2>
         <Link to="/contact" className="flat-button">CONTACT ME</Link>
       </div>
+      <Logo />
     </div>
   )
 }
